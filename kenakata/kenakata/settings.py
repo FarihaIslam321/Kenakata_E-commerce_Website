2d2ac42c0d9081a66bd8e9000db7dee6753a7a4a
Rishat2025 <rishatsultan09@gmail.com>
2025-11-24 22:23:41 +0600

    Added the home page and necessasry settings

--- kenakata/kenakata/settings.py
+++ kenakata/kenakata/settings.py
@@ -55,6 +55,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
+        'DIRS': [BASE_DIR / 'templates'],  # 👈 add this line
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -114,7 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # 👈 add this line
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # optional (for deployment)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
